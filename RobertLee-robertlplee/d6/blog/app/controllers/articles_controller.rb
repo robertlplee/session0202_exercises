64f9ec59ef08cecec407243b23358cc3bd6e3458
Robert <robertlplee@gmail.com>
2015-06-22 22:35:35 +0200

    finished lab d6

--- RobertLee-robertlplee/d6/blog/app/controllers/articles_controller.rb
+++ RobertLee-robertlplee/d6/blog/app/controllers/articles_controller.rb
@@ -1,4 +1,11 @@
 class ArticlesController < ApplicationController
+ 
+	http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+ 
+	def index
+		@articles = Article.all
+	end
+
 	def index
 		@articles = Article.all
 	end
@@ -34,7 +41,14 @@ class ArticlesController < ApplicationController
 			render 'edit'
 		end
 	end
-	
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+ 
+		redirect_to articles_path
+	end
+
 end
 
 private
