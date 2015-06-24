48ac2845d731dd6e0e5c63afdfb7941be7dd3b4a
Robert <robertlplee@gmail.com>
2015-06-24 17:55:22 +0200

    lekker plekke done

--- RobertLee-robertlplee/d8/lekker_plekke/app/controllers/places_controller.rb
+++ RobertLee-robertlplee/d8/lekker_plekke/app/controllers/places_controller.rb
@@ -4,53 +4,47 @@ class PlacesController < ApplicationController
 	end
 
 	def show
-		@places = Place.find(params[:id])
+		@place = Place.find(params[:id])
 	end
 
 	def new
-		@places = Place.new
+		@place = Place.new
 	end
 
 	def create
-		@places = Place.new(places_params)
+		@place = Place.new(place_params)
 	
-		if @places.save
-			redirect_to @places
+		if @place.save
+			redirect_to @place
 		else
 			render "new"
 		end
 	end
 
 	def edit
-		@places = Place.find(params[:id])
-
-		if @places.update(animal_params)
-			redirect_to @places
-		else
-			render 'edit'
-		end
+		@place = Place.find(params[:id])
 	end
 
 	def update
-		@places = Place.find(params[:id])
+		@place = Place.find(params[:id])
 
-		if @places.update(article_params)
-			redirect_to @places
+		if @place.update(place_params)
+			redirect_to @place
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
-		@places = Place.find(params[:id])
-		@places.destroy
+		@place = Place.find(params[:id])
+		@place.destroy
  
-		redirect_to places_path
+		redirect_to place_path
 	end
 
 private 
-	def places_params
-		params.require(:places).permit(:name, :region, :life_expectancy, :conservation_status)
+	def place_params
+		params.require(:place).permit(:name, :location, :description)
 	end
 	
 end
